629c940609250160176841d82b8234548b99bf31
William Robertson <c.william.robertson@gmail.com>
2024-01-24 18:56:37 +0100

    Minimize and wrap icons

--- packages/ui/src/icons/singleAction.tsx
+++ packages/ui/src/icons/singleAction.tsx
@@ -1,22 +1,17 @@
+import { Icon } from '@chakra-ui/react'
+
 export function SingleAction(): JSX.Element {
   return (
-    <svg
-      aria-label="Single Action"
-      fill="none"
-      height="1.5em"
-      viewBox="0 0 12 12"
-      width="1.5em"
-      xmlns="http://www.w3.org/2000/svg"
-    >
+    <Icon boxSize={6} viewBox="0 0 12 12">
       <title id="reaction-icon">Single Action</title>
       <path
-        d="M3.02482 9.05985L6.15664 5.96429L3.03199 2.88564L5.93579 0L12 6.02023C9.99838 8.00645 7.96741 10.0218 5.97383 12C5.04827 11.0772 4.07255 10.1044 3.02482 9.05985Z"
-        fill="#fff"
-       />
+        d="m3.025 9.06 3.132-3.096-3.125-3.078L5.936 0 12 6.02 5.974 12c-.926-.923-1.901-1.896-2.95-2.94Z"
+        fill="currentColor"
+      />
       <path
-        d="M2.02483 8.07429L0 6.05694L2.05458 3.99999L4.08045 6.02555L2.02483 8.07429Z"
-        fill="#fff"
-       />
-    </svg>
+        d="M2.025 8.074 0 6.057 2.055 4 4.08 6.026 2.025 8.074Z"
+        fill="currentColor"
+      />
+    </Icon>
   )
 }
--- packages/ui/src/theme/themeProvider.tsx
+++ packages/ui/src/theme/themeProvider.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import type { PropsWithChildren } from 'react'
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 
