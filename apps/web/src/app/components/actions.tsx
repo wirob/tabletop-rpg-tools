32f1afa2807c78ddab222fe63491342cac7b77cf
William Robertson <c.william.robertson@gmail.com>
2024-01-28 11:21:40 +0100

    Move actions to components

---
+++ apps/web/src/app/components/actions.tsx
@@ -0,0 +1,110 @@
+'use client'
+
+import { useState } from 'react'
+import {
+  FreeAction as FreeActionIcon,
+  SingleAction as SingleActionIcon,
+  TripleAction as TripleActionIcon,
+  Reaction as ReactionIcon,
+  DoubleAction as DoubleActionIcon,
+} from '@repo/ui/icons'
+import { Button, IconButton } from '@repo/ui/chakra'
+
+type ActionsAvailable = {
+  [key in Action]: boolean
+}
+
+export default function Actions(): JSX.Element {
+  const [actionsUsed, setActionsUsed] = useState(0)
+  const [actions, setActions] = useState<ActionsAvailable>({
+    free: false,
+    single: false,
+    double: false,
+    triple: false,
+    reaction: false,
+  })
+
+  const handleClick: (action: Action, cost: number) => void = (
+    action,
+    cost
+  ) => {
+    const actionsCost = actionsUsed + cost
+    const actionsTaken = { ...actions }
+
+    if (action === 'reaction') actionsTaken.reaction = true
+    if (actionsCost >= 1) actionsTaken.triple = true
+    if (actionsCost >= 2) actionsTaken.double = true
+    if (actionsCost >= 3) actionsTaken.single = true
+
+    setActionsUsed(actionsCost)
+    setActions(actionsTaken)
+  }
+
+  const handleReset: () => void = () => {
+    setActions({
+      free: false,
+      single: false,
+      double: false,
+      triple: false,
+      reaction: false,
+    })
+    setActionsUsed(0)
+  }
+
+  return (
+    <div className="grid gap-2 grid-cols-6">
+      <IconButton
+        aria-label="free action"
+        icon={<FreeActionIcon />}
+        isDisabled={actions.free}
+        onClick={() => {
+          handleClick('free', 0)
+        }}
+      />
+
+      <IconButton
+        aria-label="single action"
+        icon={<SingleActionIcon />}
+        isDisabled={actions.single}
+        onClick={() => {
+          handleClick('single', 1)
+        }}
+      />
+
+      <IconButton
+        aria-label="double action"
+        icon={<DoubleActionIcon />}
+        isDisabled={actions.double}
+        onClick={() => {
+          handleClick('double', 2)
+        }}
+      />
+
+      <IconButton
+        aria-label="triple action"
+        icon={<TripleActionIcon />}
+        isDisabled={actions.triple}
+        onClick={() => {
+          handleClick('triple', 3)
+        }}
+      />
+
+      <IconButton
+        aria-label="reaction"
+        icon={<ReactionIcon />}
+        isDisabled={actions.reaction}
+        onClick={() => {
+          handleClick('reaction', 0)
+        }}
+      />
+
+      <Button
+        onClick={() => {
+          handleReset()
+        }}
+      >
+        Reset
+      </Button>
+    </div>
+  )
+}
