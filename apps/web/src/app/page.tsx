31f730b2a0078cbf2ecba28cca059b1b6bdfa071
William Robertson <c.william.robertson@gmail.com>
2024-01-29 18:14:18 +0100

    Feat/hp manager (#5)
    * Component for managing health points
    
    * Adds chakra icons to ui
    
    * Exports chakra icons from ui

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,6 @@
 import Actions from './components/actions'
 import Experience from './components/experience'
+import HealthPoints from './components/healthPoints'
 import styles from './page.module.css'
 
 export default function Page(): JSX.Element {
@@ -7,6 +8,7 @@ export default function Page(): JSX.Element {
     <main className={styles.main}>
       <Actions />
       <Experience />
+      <HealthPoints />
     </main>
   )
 }
