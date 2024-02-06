40e6aac1309105a1555359d35c5daa0f4a73ebea
William Robertson <c.william.robertson@gmail.com>
2024-02-06 16:47:33 +0100

    Creates context

---
+++ apps/web/src/app/context/toolsVisibilityContext.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { createContext, useContext, type PropsWithChildren } from 'react'
+import { useLocalStorage } from 'usehooks-ts'
+
+interface ToolsVisibilityContextType {
+  toolsVisibility: ToolsVisibility
+  setToolsVisibility: (name: ToolNames, value: boolean) => void
+}
+
+const ToolsVisibilityContext = createContext<ToolsVisibilityContextType>(
+  {} as ToolsVisibilityContextType
+)
+
+export function ToolsVisibilityProvider({
+  children,
+}: PropsWithChildren): JSX.Element {
+  const [toolsVisibility, setToolsVisibility] =
+    useLocalStorage<ToolsVisibility>('toolsVisibility', {
+      actions: true,
+      experience: true,
+      health: true,
+      notes: true,
+    })
+
+  const handleChange: (name: ToolNames, value: boolean) => void = (
+    name: string,
+    value: boolean
+  ) => {
+    setToolsVisibility((prev) => ({ ...prev, [name]: value }))
+  }
+
+  return (
+    <ToolsVisibilityContext.Provider
+      value={{ toolsVisibility, setToolsVisibility: handleChange }}
+    >
+      {children}
+    </ToolsVisibilityContext.Provider>
+  )
+}
+
+export function useToolsVisibility(): ToolsVisibilityContextType {
+  return useContext(ToolsVisibilityContext)
+}
