6160c7a45976ae56b01a5598976f7ebac3fc3771
William Robertson <c.william.robertson@gmail.com>
2024-03-12 22:02:22 +0100

    Settings handles temp health

--- apps/web/src/components/health/healthSettings.tsx
+++ apps/web/src/components/health/healthSettings.tsx
@@ -21,40 +21,58 @@ import { useState } from 'react'
 
 interface HealthSettingsProps {
   healthMax: number
-  setHealthMax: (max: number) => void
+  setHealthMax: (hp: number) => void
+  setTempHealth: (hp: number) => void
+  tempHealth: number
 }
 
-type HandleChange = (val: number) => void
+type HandleChange = (
+  val: number,
+  onInvalidCallback: (val: boolean) => void,
+  setValueCallback: (val: number) => void
+) => void
 type HandleSave = () => void
 
 function HealthSettings(props: HealthSettingsProps): JSX.Element {
-  const { healthMax, setHealthMax } = props
+  const { healthMax, setHealthMax, setTempHealth, tempHealth } = props
   const { isOpen, onToggle, onClose } = useDisclosure()
   const [newHealthMax, setNewHealthMax] = useState(healthMax)
-  const [invalid, setInvalid] = useState(false)
+  const [newTempHealth, setNewTempHealth] = useState(tempHealth)
+  const [maxHealthInvalid, setMaxHealthInvalid] = useState(false)
+  const [tempHealthInvalid, setTempHealthInvalid] = useState(false)
   const [disabled, setDisabled] = useState(true)
 
-  const handleChange: HandleChange = (val) => {
+  const handleChange: HandleChange = (
+    val,
+    onInvalidCallback,
+    setValueCallback
+  ) => {
     if (isNaN(val) || !Number.isInteger(val)) {
-      setInvalid(true)
+      onInvalidCallback(true)
       setDisabled(true)
       return
     }
 
-    setInvalid(false)
+    onInvalidCallback(false)
     setDisabled(false)
-    setNewHealthMax(val)
+    setValueCallback(val)
   }
 
   const handleSave: HandleSave = () => {
     setHealthMax(newHealthMax)
+    setTempHealth(newTempHealth)
     onToggle()
   }
 
   return (
     <Popover isOpen={isOpen} onClose={onClose} placement="right-start">
       <PopoverTrigger>
-        <IconButton aria-label="health settings" isRound onClick={onToggle} size="xs">
+        <IconButton
+          aria-label="health settings"
+          isRound
+          onClick={onToggle}
+          size="xs"
+        >
           <SettingsIcon />
         </IconButton>
       </PopoverTrigger>
@@ -63,10 +81,30 @@ function HealthSettings(props: HealthSettingsProps): JSX.Element {
           <FormControl>
             <FormLabel>Max HP</FormLabel>
             <NumberInput
-              isInvalid={invalid}
+              isInvalid={maxHealthInvalid}
               min={1}
               onChange={(_, valAsNumber) => {
-                handleChange(valAsNumber)
+                handleChange(valAsNumber, setMaxHealthInvalid, setNewHealthMax)
+              }}
+              value={newHealthMax}
+            >
+              <NumberInputField />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
+            <FormLabel>Temp HP</FormLabel>
+            <NumberInput
+              defaultValue={newTempHealth}
+              isInvalid={tempHealthInvalid}
+              min={1}
+              onChange={(_, valAsNumber) => {
+                handleChange(
+                  valAsNumber,
+                  setTempHealthInvalid,
+                  setNewTempHealth
+                )
               }}
             >
               <NumberInputField />
