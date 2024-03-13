01789aa97ba890ca08e513686838970ac5975e2a
William Robertson <c.william.robertson@gmail.com>
2024-03-13 12:39:05 +0100

    Feature/tab 41 exp visual improvement (#57)
    * Change complex button text to "save"
    
    * Remove max exp cap
    
    * Reset input field after save
    
    * Layout changes
    
    * Move invalid check to a useEffect
    
    * Remove on invalid check in input field

--- apps/web/src/components/experience.tsx
+++ apps/web/src/components/experience.tsx
@@ -3,13 +3,16 @@
 import { useEffect, useState } from 'react'
 import { useLocalStorage } from 'usehooks-ts'
 import {
+  Box,
   Button,
+  Flex,
+  Grid,
+  GridItem,
   NumberInput,
   NumberInputField,
   NumberInputStepper,
   NumberIncrementStepper,
   NumberDecrementStepper,
-  SimpleGrid,
   Stack,
   Stat,
   StatLabel,
@@ -17,85 +20,75 @@ import {
 } from '@repo/ui/chakra'
 import { useToolsVisibility } from '@/app/context/toolsVisibilityContext'
 
-type ExpButtonText = 'Add exp' | 'Remove exp'
-
 export default function Experience(): JSX.Element | null {
-  const maxExp = 999
-
-  const [expButtonText, setExpButtonText] = useState<ExpButtonText>('Add exp')
   const [currentExp, setCurrentExp] = useLocalStorage('userExp', 0)
   const [invalidInput, setInvalidInput] = useState(true)
   const [expToSet, setExpToSet] = useState(0)
   const { toolsVisibility } = useToolsVisibility()
 
   const handleChange: (exp: number) => void = (exp) => {
-    if (isNaN(exp)) {
-      setInvalidInput(true)
-      return
-    }
-
-    setExpToSet(exp)
-
-    if (exp <= maxExp) {
-      setInvalidInput(false)
-    }
-  }
-
-  useEffect(() => {
-    if (expToSet < 0) setExpButtonText('Remove exp')
-    else setExpButtonText('Add exp')
-  }, [expToSet])
-
-  const handleOnInvalid: () => void = () => {
-    setInvalidInput(true)
+    if (isNaN(exp)) setExpToSet(0)
+    else setExpToSet(exp)
   }
 
   const handleAddExp: () => void = () => {
+    if (!expToSet) return
+
     setCurrentExp(currentExp + expToSet)
+    setExpToSet(0)
   }
+
+  useEffect(() => {
+    if (expToSet === 0) setInvalidInput(true)
+    else setInvalidInput(false)
+  }, [setInvalidInput, expToSet])
 
   if (!toolsVisibility.experience) return null
 
   return (
-    <SimpleGrid className="mt-4" columns={3} spacing={2}>
-      <Stat>
-        <StatNumber>{currentExp}</StatNumber>
-        <StatLabel>EXP</StatLabel>
-      </Stat>
-      <Stack>
-        <NumberInput
-          clampValueOnBlur={false}
-          inputMode="numeric"
-          keepWithinRange={false}
-          max={maxExp}
-          maxW={20}
-          onChange={(_, valueAsNumber) => {
-            handleChange(valueAsNumber)
+    <Grid gap={6} marginTop={8} templateColumns="repeat(3, 1fr)">
+      <GridItem>
+        <Flex justifyContent="end">
+          <Box>
+            <Stat>
+              <StatNumber>{currentExp}</StatNumber>
+              <StatLabel>EXP</StatLabel>
+            </Stat>
+          </Box>
+        </Flex>
+      </GridItem>
+      <GridItem>
+        <Stack>
+          <NumberInput
+            clampValueOnBlur={false}
+            inputMode="numeric"
+            keepWithinRange={false}
+            maxW={24}
+            onChange={(_, valueAsNumber) => {
+              handleChange(valueAsNumber)
+            }}
+            size="md"
+            step={10}
+            value={expToSet}
+          >
+            <NumberInputField />
+            <NumberInputStepper>
+              <NumberIncrementStepper>+</NumberIncrementStepper>
+              <NumberDecrementStepper>-</NumberDecrementStepper>
+            </NumberInputStepper>
+          </NumberInput>
+        </Stack>
+      </GridItem>
+      <GridItem>
+        <Button
+          isDisabled={invalidInput}
+          onClick={() => {
+            handleAddExp()
           }}
-          onInvalid={() => {
-            handleOnInvalid()
-          }}
-          size="xs"
-          step={10}
         >
-          <NumberInputField />
-          <NumberInputStepper>
-            <NumberIncrementStepper />
-            <NumberDecrementStepper />
-          </NumberInputStepper>
-        </NumberInput>
-      </Stack>
-
-      <Button
-        colorScheme="green"
-        isDisabled={invalidInput}
-        onClick={() => {
-          handleAddExp()
-        }}
-        size="xs"
-      >
-        {expButtonText}
-      </Button>
-    </SimpleGrid>
+          Save
+        </Button>
+      </GridItem>
+    </Grid>
   )
 }
