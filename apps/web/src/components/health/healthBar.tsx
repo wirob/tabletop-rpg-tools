2942126c16b4caf6d2fe34ce63ffbbc5e0e7b30d
William Robertson <c.william.robertson@gmail.com>
2024-03-17 17:58:16 +0100

    Correct hex

--- apps/web/src/components/health/healthBar.tsx
+++ apps/web/src/components/health/healthBar.tsx
@@ -14,7 +14,7 @@ function HealthBar(props: HealthBarProps): JSX.Element {
   const green = useColorModeValue('green.500', 'green.200')
   const midGreenOrange = useColorModeValue('#81bf2f', '#c3f194')
   const orange = useColorModeValue('orange.500', 'orange.200')
-  const midOrangeRed = useColorModeValue('#e1522f', '#e1522f')
+  const midOrangeRed = useColorModeValue('#e1522f', '#fdbd9f')
   const red = useColorModeValue('red.500', 'red.200')
   const grey = useColorModeValue('gray.100', 'whiteAlpha.200')
 
