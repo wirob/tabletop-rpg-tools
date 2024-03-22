3385fde6bbaa76c81234cb1a21075504ef57827b
William Robertson <c.william.robertson@gmail.com>
2024-03-22 17:08:01 +0100

    Moves component settings to own dir and breaks out health settings (#70)

---
+++ apps/web/src/components/sideBar/componentSettings/componentSettings.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import HealthSettings from './healthSettings'
+
+function ComponentSettings(): JSX.Element {
+  return (
+    <>
+      <HealthSettings />
+    </>
+  )
+}
+
+export default ComponentSettings
