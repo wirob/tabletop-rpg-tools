f64dc6ae7f2540d2c5549e180cebde765ec3ec50
William Robertson <c.william.robertson@gmail.com>
2024-03-25 17:28:21 +0100

    Feature/tab 56 create setting for exp (#71)
    * Increment can be undefined or a number
    
    * Uses component settings hook
    
    * Adds exp setting
    
    * Exp setting component

--- apps/web/src/app/context/componentSettingsContext.tsx
+++ apps/web/src/app/context/componentSettingsContext.tsx
@@ -4,10 +4,13 @@ import { createContext, useContext, type PropsWithChildren } from 'react'
 import { useLocalStorage } from 'usehooks-ts'
 
 type HandleHealthChange = (value: HealthSettings) => void
+type HandleExperienceChange = (value: ExperienceSettings) => void
 
 interface ComponentSettingsContextType {
   setHealthSettings: HandleHealthChange
   healthSettings: HealthSettings
+  experienceSettings: ExperienceSettings
+  setExperienceSettings: HandleExperienceChange
 }
 
 const ComponentSettingsContext =
@@ -29,12 +32,21 @@ export function ComponentSettingsProvider({
   )
 
   const handleHealthChange: HandleHealthChange = (health) => {
-    setComponentSettings((prev) => ({...prev, health}))
+    setComponentSettings((prev) => ({ ...prev, health }))
+  }
+
+  const handleExperienceChange: HandleExperienceChange = (experience) => {
+    setComponentSettings((prev) => ({ ...prev, experience }))
   }
 
   return (
     <ComponentSettingsContext.Provider
-      value={{ healthSettings: componentSettings.health, setHealthSettings: handleHealthChange }}
+      value={{
+        healthSettings: componentSettings.health,
+        setHealthSettings: handleHealthChange,
+        experienceSettings: componentSettings.experience,
+        setExperienceSettings: handleExperienceChange,
+      }}
     >
       {children}
     </ComponentSettingsContext.Provider>
--- apps/web/src/components/sideBar/componentSettings/componentSettings.tsx
+++ apps/web/src/components/sideBar/componentSettings/componentSettings.tsx
@@ -1,11 +1,13 @@
 'use client'
 
+import ExpSettings from './expSettings'
 import HealthSettings from './healthSettings'
 
 function ComponentSettings(): JSX.Element {
   return (
     <>
       <HealthSettings />
+      <ExpSettings />
     </>
   )
 }
