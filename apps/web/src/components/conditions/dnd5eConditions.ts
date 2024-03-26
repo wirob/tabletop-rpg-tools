c42878f63710d2ffd566de609195f517f8e54434
William Robertson <c.william.robertson@gmail.com>
2024-03-26 11:10:29 +0100

    Adds dnd 5e conditions

---
+++ apps/web/src/components/conditions/dnd5eConditions.ts
@@ -0,0 +1,18 @@
+const dnd5eConditions = [
+  'Blinded',
+  'Charmed',
+  'Deafened',
+  'Exhaustion',
+  'Frightened',
+  'Grappled',
+  'Incapacitated',
+  'Paralyzed',
+  'Petrified',
+  'Poisoned',
+  'Prone',
+  'Restrained',
+  'Stunned',
+  'Unconscious',
+]
+
+export default dnd5eConditions
